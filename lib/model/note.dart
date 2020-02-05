c6e18fc7d92a5d57f75818d2a8215b8391e64650
Vivek Kumar Gupta <vivekkumargupta680@gmail.com>
2020-02-05 16:15:44 +0530

    modified model for sqlite integration

--- lib/model/note.dart
+++ lib/model/note.dart
@@ -3,8 +3,63 @@ class Note {
   int _priority;
   String _title;
   String _description;
-  DateTime _createdAt;
-  DateTime _modifiedAt;
+  int _createdAt;
+  int _modifiedAt;
 
-  Note(this._title, this._description, this._priority);
+  Note(this._title, this._priority, this._createdAt, [this._description]);
+  Note.withId(this._id, this._title, this._priority, this._modifiedAt, [this._description]);
+
+  int get id => _id;
+  String get title => _title;
+  String get description => _description;
+  int get createdAt => _createdAt;
+  int get modifiedAt => _modifiedAt;
+  int get priority => _priority;
+
+  set title(newTitle) {
+    if ( newTitle.length < 255 ) {
+      this._title = newTitle;
+    }
+  }
+  set description(newMessage) {
+    if ( newMessage.length < 255 ) {
+      this._description = newMessage;
+    }
+  }
+  set priority(newP) {
+    if ( newP >= 0 && newP <= 10 ) {
+      this._priority = newP;
+    }
+  }
+  set createdAt(newD) {
+    this._createdAt = newD;
+  }
+  set modifiedAt(newD) {
+    this._modifiedAt = newD;
+  }
+
+  // Convert a Note Object to Map Object
+  Map<String, dynamic> toMap() {
+    var map = Map<String, dynamic> ();
+    if ( _id != null ) {
+      map['id'] = _id;
+    }
+    map['title'] = _title;
+    map['description'] = _description;
+    map['priority'] = _priority;
+    map['createdAt'] = _createdAt;
+    map['modifiedAt'] = _modifiedAt;
+    return map;
+  }
+
+  // Extract a Note Object from a Map Object
+  Note.fromMapObject( Map<String, dynamic> map ) {
+    this._id = map['id'];
+    this._createdAt = map['createdAt'];
+    this._modifiedAt = map["modifiedAt"];
+    this._priority = map['priority'];
+    this._title = map['title'];
+    this._description = map['description'];
+  }
+
 }
--- lib/view/note_list.dart
+++ lib/view/note_list.dart
@@ -1,18 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:note_keeper/model/note.dart';
 
 class NoteList extends StatefulWidget {
   NoteListState createState() => NoteListState();
 }
 
 class NoteListState extends State<NoteList> {
+
+  List<Note> notes;
+
+  @override
+  void initState() {
+    notes = List<Note> ();
+    super.initState();
+  }
+
+  Widget _buildNotes() {
+    return ListView.builder(
+      itemCount: notes.length,
+      itemBuilder: (BuildContext context, int pos) {
+        double p = notes[pos].id*1.0 / 10;
+        return Card(
+          color: Colors.redAccent,
+          elevation: 2.0,
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Color.fromARGB(255, 255*p as int, 255*(1-p) as int, 0),
+              child: Icon(Icons.event_note),
+            ),
+            title: Text(notes[pos].title),
+            subtitle: Text(notes[pos].description),
+            onTap: () {
+              _gotoDetails(true, notes[pos]);
+            },
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Note Keeper"),
       ),
-      body: Container(),
+      body: _buildNotes(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          _gotoDetails(false, null);
+        },
+      ),
     );
+  }
+
+  _gotoDetails(bool isEdit, Note note) async {
+    print("");
   }
 
 }
